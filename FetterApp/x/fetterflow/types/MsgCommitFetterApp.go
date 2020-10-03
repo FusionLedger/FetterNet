e496720ae9cb6ab9fc5bdc77757b36a68d2e0aa9
Gokul Alex <gokulsan@users.noreply.github.com>
2020-10-03 23:50:05 +0530

    Create MsgCommitFetterApp.go

---
+++ FetterApp/x/fetterflow/types/MsgCommitFetterApp.go
@@ -0,0 +1,56 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// MsgCommitFetterApp
+// ------------------------------------------------------------------------------
+var _ sdk.Msg = &MsgCommitFetterApp{}
+
+// MsgCommitSolution - struct for unjailing jailed validator
+type MsgCommitFetterApp struct {
+	Scavenger             sdk.AccAddress `json:"scavenger" yaml:"scavenger"`                         // address of the scavenger
+	SolutionHash          string         `json:"solutionhash" yaml:"solutionhash"`                   // solutionhash of the scavenge
+	SolutionScavengerHash string         `json:"solutionScavengerHash" yaml:"solutionScavengerHash"` // solution hash of the scavenge
+}
+
+// NewMsgCommitFetterApp creates a new MsgCommitSolution instance
+func NewMsgCommitFetterApp(scavenger sdk.AccAddress, solutionHash string, solutionScavengerHash string) MsgCommitFetterApp {
+	return MsgCommitFetterApp{
+		Scavenger:             scavenger,
+		SolutionHash:          solutionHash,
+		SolutionScavengerHash: solutionScavengerHash,
+	}
+}
+
+// CommitSolutionConst is CommitSolution Constant
+const CommitSolutionConst = "CommitSolution"
+
+// nolint
+func (msg MsgCommitFetterApp) Route() string { return RouterKey }
+func (msg MsgCommitFetterApp) Type() string  { return CommitSolutionConst }
+func (msg MsgCommitFetterApp) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{sdk.AccAddress(msg.Scavenger)}
+}
+
+// GetSignBytes gets the bytes for the message signer to sign on
+func (msg MsgCommitSolution) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// ValidateBasic validity check for the AnteHandler
+func (msg MsgCommitFetterApp) ValidateBasic() error {
+	if msg.Scavenger.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "creator can't be empty")
+	}
+	if msg.SolutionHash == "" {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "solutionHash can't be empty")
+	}
+	if msg.SolutionScavengerHash == "" {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "solutionScavengerHash can't be empty")
+	}
+	return nil
+}
